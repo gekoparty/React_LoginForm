5bec1ea816c2a2f96024691fec348a7e4605ec12
gekoparty <gekoparty@hotmail.com>
2021-09-24 16:57:04 +0200

    Project Setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
 import '../styles/index.scss';
 
 
+import React, { useState } from 'react';
 
+import Login from './Login/Login';
+import Home from './Home/Home';
+import MainHeader from './MainHeader/MainHeader';
 
-const App = () => {
-    return (
-        <div>
-            <div className="container">
-            
-        </div>
-        </div>
-    )
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
+
